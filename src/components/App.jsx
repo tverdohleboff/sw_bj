729c7ef38b3eae28fdb22cde1137e1038d5d4aae
Alex <tverdohleboff@gmail.com>
2023-05-06 14:24:45 +0400

    Add interactive on mainPage (switch background color on start game)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,33 +9,51 @@ function App() {
 
   const count = useSelector((state) => state.counter.value);
   const dispatch = useDispatch();
+  let isReady = false;
 
   async function savePeople() {
     const ready = await downloadPeople();
-    console.log("ready", ready);
     dispatch(save(ready));
+    switchToReady();
+  }
+
+  const switchToReady = () => {
+    isReady = true;
+    document.querySelector('.downloadButton').classList.remove('visible');
+    document.querySelector('.downloadButton').classList.add('invisible');
+    document.querySelector('.App').classList.add('purple');
   }
 
   return (
     <div className='App'>
       <div>
+        <h1 className='hReady invisible'>Ready!</h1>
         <button
+          className='invisible'
           aria-label="Increment value"
           onClick={() => dispatch(increment())}
         >
           Прибавить
         </button>
-        <span>{count}</span>
+        <span className='invisible'>{count}</span>
         <button
+          className='invisible'
           aria-label="Decrement value"
           onClick={() => dispatch(decrement())}
         >
           Отнять
         </button>
+        <button
+          className='invisible'
+          onClick={() =>
+            dispatch(incrementByAmount(22))}>Прибавить 22
+        </button>
         <br />
-        <button onClick={() => dispatch(incrementByAmount(22))}>Прибавить 22</button>
-        <br />
-        <button onClick={() => savePeople()}>Cкачать героев</button>
+        <button
+          className='downloadButton button bigText visible'
+          onClick={() => savePeople()}
+        >Game
+        </button>
       </div>
     </div>
   );
